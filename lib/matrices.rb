7ac156113812bb429486ef2bc3dd9ebc42e379d2
Teno <teno@ubuntu.ubuntu-domain>
2013-11-04 18:14:49 +0000

    Creado método initialize para crear matrices de tamaño y valores fijos para poder llevar a cabo pruebas

--- lib/matrices.rb
+++ lib/matrices.rb
@@ -35,10 +35,34 @@ class Matriz
 		raise unless filas.is_a?(Numeric)
 		raise unless columnas.is_a?(Numeric)
 		
-		puts "Elementos de la matriz"
+		puts "Elementos de la matriz: "
 		asignar_matriz_teclado(@matriz, @filas)
 		mostrar(@matriz)
 		
+		
+	end
+	
+	def initialize(fil_col)
+
+		#comprobamos que los datos de entradas sean numericos
+		raise unless fil_col.is_a?(Numeric)
+		
+		#inicializamos las variables
+		@filas, @columnas = fil_col, fil_col
+		@matriz = []
+		
+		#inicializamos valores fijos de matriz
+		valor = 1
+		for i in (0..fil_col-1)
+				@matriz[i] = []
+				for j in (0..fil_col-1)
+						@matriz[i][j] = valor
+						valor +=  1
+				end
+		end
+		
+		puts "Elementos de la matriz: "
+		mostrar(@matriz)
 		
 	end
 	
@@ -70,7 +94,56 @@ class Matriz
 		end
 	end
 	
+	def * (other)
+		c = []
+		i = 0
+		while( i < @filas )
+			j = 0
+			fila = []
+			while( j < @columnas )
+				k = 0
+				fila[j] = 0
+				while( k < @filas)
+					fila[j] += (@matriz[i][k] * other.matriz[k][j] )
+					k += 1
+				end
+				j += 1
+			end
+			c << fila
+			i += 1
+		end
+		mostrar(c)
+		return c
+	end
+	
+	def + (other)
+	c = []
+		i = 0
+		while(i < @filas)
+			j = 0
+			fila = []
+			while(j < @columnas)
+				fila[j] = @matriz[i][j] + other.matriz[i][j]				
+				j += 1
+			end
+		c << fila
+		i += 1	
+		end
+		mostrar(c)
+		return c
+
+	end
+	
 end
 
-m = Matriz.new
-	
+
+m1 = Matriz.new(2)
+m2 = Matriz.new(2)
+
+print "\n"
+puts "Suma de matrices:"
+m1 + m2
+
+print "\n"
+puts "Producto de matrices:"
+m1 * m2
