27a0038eb12a495b30b2ba8cc73893363e0038b4
Teno <teno@ubuntu.ubuntu-domain>
2013-11-05 14:37:42 +0000

    Añadido prueba de almacenamiento de variables

--- spec/matrices_spec.rb
+++ spec/matrices_spec.rb
@@ -4,5 +4,17 @@ describe Matriz do
 	before :each do
 		@m1 = Matriz.new(3)
 		@m2 = Fraccion.new(3)
-    end 
+    end
+    
+    describe "# Almacenamiento del numero de filas y columnas" do
+    	it "Se almacena el numero de filas" do
+      		@m1.filas.should eq(3)
+   		end
+   		
+   		it "Se almacena el numero de columnas" do
+			@m2.columnas.should eq(3)
+		end
+   	end
+   	
+
 end
